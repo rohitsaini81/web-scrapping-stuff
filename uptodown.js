1bd5d3a92de31eca5e6ad543adb4b9977613c374
rick <rohit1322saini@gmail.com>
2025-11-28 21:34:56 +0530

    rename

--- uptodown.js
+++ uptodown.js
@@ -1,11 +1,11 @@
- listed all apps of uptodown
+/* listed all apps of uptodown
  step 1 select any random app
 https://tiktok-lite.en.uptodown.com/android
 
 step 2 get that download page url form ancor tag inside get latest version download button
 https://tiktok.en.uptodown.com/android/download
 
-step 3 extract download token and fetch download link using this function :
+step 3 extract download token and fetch download link using this function :*/
 (function() {
   const token = document.querySelector('#detail-download-button').dataset.url;
   const url = "https://dw.uptodown.net/dwn/" + token;
