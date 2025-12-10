cfcf9e811cb214a855a422c17e8f031ebdd0e00e
rick <rohit1322saini@gmail.com>
2025-12-10 22:08:54 +0530

    app query page

--- astro_project/src/pages/rss.xml.js
+++ astro_project/src/pages/rss.xml.js
@@ -2,8 +2,24 @@ import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
+// export async function GET(context) {
+// 	const posts = await getCollection('blog');
+// 	// const blog_posts = await fetch("http://localhost:5000/api/blogs");
+// 	// console.log(blog_posts)
+// 	return rss({
+// 		title: SITE_TITLE,
+// 		description: SITE_DESCRIPTION,
+// 		site: context.site,
+// 		items: posts.map((post) => ({
+// 			...post.data,
+// 			link: `/blog/${post.id}/`,
+// 		})),
+// 	});
+// }
+
+
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const posts = await getCollection('apps');
 	// const blog_posts = await fetch("http://localhost:5000/api/blogs");
 	// console.log(blog_posts)
 	return rss({
@@ -12,7 +28,7 @@ export async function GET(context) {
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/blog/${post.id}/`,
+			link: `/apps/${post.id}/`,
 		})),
 	});
 }
